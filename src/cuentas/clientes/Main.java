23d5b85e183be1c5db6177c48a7dc45ed770cf36
Miguel_Agustin <miguel-agus1995@hotmail.com>
2025-02-15 09:44:12 +0100

    Segundos cambios realizados

--- src/cuentas/clientes/Main.java
+++ src/cuentas/clientes/Main.java
@@ -11,13 +11,15 @@ public class Main {
     public static void operativa_cuenta(float cantidad) {
         Cuenta cuenta1;
         double saldoActual;
+        
+        System.out.println("Bienvenido ");
 
         cuenta1 = new Cuenta("Paco Perez","1000-2365-85-1230456789",2500,0);
         
         System.out.println("El saldo actual es:"+ cuenta1.estado() );
 
         try {
-            cuenta1.retirar(2300);
+            cuenta1.retirar(1);
             System.out.println("El saldo actual es:"+ cuenta1.estado() );
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
